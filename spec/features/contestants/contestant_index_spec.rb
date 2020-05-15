482eec6eab641808949bfa7e383126a2261d204b
Alex Pariseau <arpariseau@gmail.com>
2020-05-15 09:40:51 -0600

    Spec for contestant index page

--- spec/features/contestants/contestant_index_spec.rb
+++ spec/features/contestants/contestant_index_spec.rb
@@ -1,4 +1,36 @@
+require 'rails_helper'
 
+RSpec.describe "Contestant index page", type: :feature do
+  it "shows the contestant's name and projects" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+
+    visit("/contestants")
+
+    within(".contestant-#{kentaro.id}") do
+      expect(page).to have_content(kentaro.name)
+      expect(page).to have_content(boardfit.name)
+      expect(page).to have_content(upholstery_tux.name)
+    end
+
+    within(".contestant-#{jay.id}") do
+      expect(page).to have_content(jay.name)
+      expect(page).to have_content(new_chic.name)
+    end
+  end
+end
 
 # As a visitor,
 # When I visit the contestants index page ("/contestants")
--- spec/features/projects/project_show_spec.rb
+++ spec/features/projects/project_show_spec.rb
@@ -12,15 +12,6 @@ RSpec.describe "Project show page", type: :feature do
   end
 end
 
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
-# ​
-
 
 # User Story 3 of 3
 # As a visitor,
