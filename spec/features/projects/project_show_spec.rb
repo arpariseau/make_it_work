e043dc4cd025ea97a9e7bbe1d347a8964047b3bc
Alex Pariseau <arpariseau@gmail.com>
2020-05-15 10:11:46 -0600

    Spec for adding average yrs exp for contestants on a project

--- spec/features/projects/project_show_spec.rb
+++ spec/features/projects/project_show_spec.rb
@@ -4,21 +4,46 @@ RSpec.describe "Project show page", type: :feature do
   before :each do
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
-  end
-  it "shows the project's name, material and theme" do
+
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @lit_fit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @lit_fit.id)
+
     visit("/projects/#{@lit_fit.id}")
+  end
+
+  it "shows the project's name, material and theme" do
     expect(page).to have_content(@lit_fit.name)
     expect(page).to have_content(@lit_fit.material)
     expect(page).to have_content(@furniture_challenge.theme)
   end
+
   it "shows the number of contestants on the project" do
-    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
-
-    ContestantProject.create(contestant_id: kentaro.id, project_id: @lit_fit.id)
-    ContestantProject.create(contestant_id: erin.id, project_id: @lit_fit.id)
-
-    visit("/projects/#{@lit_fit.id}")
     expect(page).to have_content("Number of Contestants: 2")
   end
+
+  it "shows the average years of experience for the contestants that worked on the project" do
+    expect(page).to have_content("Average Contestant Experience: #{23.0 / 2}")
+  end
 end
+#
+# As a visitor,
+# When I visit a project's show page
+# I see the average years of experience for the contestants that worked on that project
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings
+#   Number of Contestants: 3
+#   Average Contestant Experience: 10.25 years)
+#
+# As a visitor,
+# When I visit a project's show page
+# I see a form to add a contestant to this project
+# When I fill out a field with an existing contestants id
+# And hit "Add Contestant To Project"
+# I'm taken back to the project's show page
+# And I see that the number of contestants has increased by 1
+# And when I visit the contestants index page
+# I see that project listed under that contestant's name
