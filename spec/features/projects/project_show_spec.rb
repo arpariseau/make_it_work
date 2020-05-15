86d6c06d0df4d717be5548cd36f4e8785d5719d0
Alex Pariseau <arpariseau@gmail.com>
2020-05-15 09:56:52 -0600

    Spec for seeing number of contestants on project show page

--- spec/features/projects/project_show_spec.rb
+++ spec/features/projects/project_show_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Project show page", type: :feature do
+  before :each do
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+  end
   it "shows the project's name, material and theme" do
-    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    visit("/projects/#{@lit_fit.id}")
+    expect(page).to have_content(@lit_fit.name)
+    expect(page).to have_content(@lit_fit.material)
+    expect(page).to have_content(@furniture_challenge.theme)
+  end
+  it "shows the number of contestants on the project" do
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
-    visit("/projects/#{lit_fit.id}")
-    expect(page).to have_content(lit_fit.name)
-    expect(page).to have_content(lit_fit.material)
-    expect(page).to have_content(furniture_challenge.theme)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: @lit_fit.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: @lit_fit.id)
+
+    visit("/projects/#{@lit_fit.id}")
+    expect(page).to have_content("Number of Contestants: 2")
   end
 end
-
-
-# User Story 3 of 3
-# As a visitor,
-# When I visit a project's show page
-# I see a count of the number of contestants on this project
-# ​
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings
-#   Number of Contestants: 3 )
